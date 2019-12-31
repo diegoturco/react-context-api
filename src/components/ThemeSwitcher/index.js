b7c81ef56812372ca123b750e9449720233cb3d1
Diego Turco <diego.turco@tecsinapse.com.br>
2019-12-31 18:51:27 -0300

    Example to change theme using context api

---
+++ src/components/ThemeSwitcher/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ThemeSwitcher = ({ toggleTheme }) => (
+  <button onClick={toggleTheme}>Change Theme</button>
+);
+
+export default ThemeSwitcher;
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const List = styled.ul`
+  background: ${props => props.theme.theme.background};
+  color: ${props => props.theme.theme.color};
+  font-family: sans-serif;
+  font-size: 18px;
+  li {
+    line-height: 36px;
+  }
+`;
+
+const TodoList = () => (
+  <List>
+    <li>Make coffee</li>
+    <li>To Work</li>
+    <li>Study ReactJS</li>
+  </List>
+);
+
+export default TodoList;
